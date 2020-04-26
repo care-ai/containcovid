947371bce09972fd69b2452a588bca88a4f13e9b
Evan Anderson <ejdanderson@gmail.com>
2020-04-26 01:59:35 -0700

    Adding check for methods

--- imports/api/publications/diagnosis.js
+++ imports/api/publications/diagnosis.js
@@ -4,7 +4,7 @@ import Diagnosis from "../../database/Diagnosis";
 Meteor.publish({
   diagnosticForUser: function () {
     if (!this.userId) return [];
-
+    // @TODO Restrict fields
     return Diagnosis.find({ userId: this.userId });
   },
 });
